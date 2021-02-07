9bac542eb2c261f441d6b819c920f8f314fe7b75
kouwasi <kouwasi@gmail.com>
2021-02-07 20:39:39 +0900

    不要なコード削除

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,7 @@ import Fastify from 'fastify'
 import helmet from 'fastify-helmet'
 import cors from 'fastify-cors'
 import fastifyStatic from 'fastify-static'
-import fastifyJwt from 'fastify-jwt'
-import { JWT_SECRET, SERVER_PORT, BASE_PATH } from './service/envValues'
+import { SERVER_PORT, BASE_PATH } from './service/envValues'
 import server from './$server'
 
 const fastify = Fastify({ logger: true })
@@ -15,7 +14,6 @@ fastify.register(fastifyStatic, {
   root: path.join(__dirname, 'public'),
   prefix: BASE_PATH
 })
-fastify.register(fastifyJwt, { secret: JWT_SECRET })
 
 server(fastify, { basePath: BASE_PATH })
 
--- server/types/index.ts
+++ server/types/index.ts
@@ -1,15 +1,5 @@
 import { Post } from '@prisma/client'
 
-export type UserInfo = {
-  id: string
-  name: string
-  icon: string
-}
-
-export type AuthHeader = {
-  authorization: string
-}
-
 export type PostResponse = Post & {
   image_url: string
 }
